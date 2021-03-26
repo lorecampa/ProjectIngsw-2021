539c9282801808156f50d889f9856602c75bdc77
CanaliDavide <79902786+CanaliDavide@users.noreply.github.com>
2021-03-26 16:01:41 +0100

    chore: add Market

---
+++ src/main/java/it/polimi/ingsw/model/personalBoard/market/BlueMarble.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.model.personalBoard.market;
+
+public class BlueMarble implements Marble{
+    Market market;
+
+    public BlueMarble(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void marbleAction() {
+
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/model/personalBoard/market/Market.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.model.personalBoard.market;
+
+import it.polimi.ingsw.model.resource.Resource;
+
+import java.util.ArrayList;
+
+public class Market {
+
+}
