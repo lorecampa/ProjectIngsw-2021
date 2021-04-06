cd2f74bfad8210ec86f794a6586c9177cec6c90e
CanaliDavide <79902786+CanaliDavide@users.noreply.github.com>
2021-04-06 16:14:41 +0200

    feat: add playersPersonalBoard in GameMaster

--- src/main/java/it/polimi/ingsw/model/GameMaster.java
+++ src/main/java/it/polimi/ingsw/model/GameMaster.java
@@ -1,5 +1,32 @@
 package it.polimi.ingsw.model;
 
-public class GameMaster {
+import it.polimi.ingsw.commonInterfaces.Observer;
+import it.polimi.ingsw.model.personalBoard.PersonalBoard;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GameMaster implements Observer {
+    private String currentPlayer = "";
+    private final Map<String, PersonalBoard> playersPersonalBoard = new HashMap<>();
+    private int vaticanReportReached = 0;
+
+    public void addPlayer(String nickname) throws IOException {
+        PersonalBoard playerPersonalBoard = new PersonalBoard();
+        playersPersonalBoard.put(nickname,playerPersonalBoard);
+    }
+
+    public void setCurrentPlayer(String player){
+        currentPlayer = player;
+    }
+
+    public void setVaticanReportReached(int idVaticanReport){
+        vaticanReportReached = idVaticanReport;
+    }
+
+    @Override
+    public void updateFromFaithTrack() {
+
+    }
 }
