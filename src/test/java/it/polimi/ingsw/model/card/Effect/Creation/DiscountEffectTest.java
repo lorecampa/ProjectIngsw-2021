4d2d4bd2c8684f1fefa2fd211c087983560b4325
lorecampa <lorenzo.campana@mail.polimi.it>
2021-04-16 13:17:59 +0200

    test: modify DiscountEffectTest

--- src/test/java/it/polimi/ingsw/model/card/Effect/Creation/DiscountEffectTest.java
+++ src/test/java/it/polimi/ingsw/model/card/Effect/Creation/DiscountEffectTest.java
@@ -7,15 +7,15 @@ import it.polimi.ingsw.model.resource.Resource;
 import it.polimi.ingsw.model.resource.ResourceFactory;
 import it.polimi.ingsw.model.resource.ResourceType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class DiscountEffectTest {
     ResourceManager rm = new ResourceManager();
+    ArrayList<Resource> costBuyDevelopment;
     ArrayList<Resource> discounts;
     Effect effect;
     private ArrayList<Resource> resourceArray(int coin, int shield, int servant, int stone, int faith, int any){
@@ -43,33 +43,48 @@ class DiscountEffectTest {
 
     @BeforeEach
     void init(){
-        rm.newTurn();
         //creates a discounts
-        discounts = resourceArray(2,1,0,0,0,0);
+        discounts = resourceArray(3,2,0,0,0,0);
         effect = new DiscountEffect(discounts);
         effect.attachResourceManager(rm);
         assertDoesNotThrow(()->effect.doEffect(State.CREATION_STATE));
+
+        ArrayList<Resource> resStrongbox = resourceArray(2, 1, 0,0,0,0);
+        for (Resource res: resStrongbox){
+            rm.addToStrongbox(res);
+        }
+
+        rm.newTurn();
+    }
+
+
+    @Test
+    void strangeTest(){
+        //strongbox (2 coin, 1 shield, 0 stone, 0 servant)
+        //discounts (3 coin, 2 shield, 0 stone, 0 servant)
+
+        costBuyDevelopment = resourceArray(4,2,0,0,0,2);
+        assertTrue(rm.canIAfford(costBuyDevelopment, true));
     }
 
 
     @ParameterizedTest
     @ValueSource(ints = {0,1,2, 3})
     void discountWithNoResources(int index) {
-        ArrayList<Resource> costBuyDevelopment;
+        //strongbox (2 coin, 1 shield, 0 stone, 0 servant)
+        //discounts (3 coin, 2 shield, 0 stone, 0 servant)
         switch (index){
             case 0:
-                //create a requirementCost of a development card
-                costBuyDevelopment = resourceArray(1,0,0,0,0,1);
-                //modify canIAfford
+                costBuyDevelopment = resourceArray(5,0,0,0,0,3);
                 assertTrue(rm.canIAfford(costBuyDevelopment, true));
                 break;
             case 1:
-                costBuyDevelopment = resourceArray(2,0,0,0,0,2);
+                costBuyDevelopment = resourceArray(6,0,0,0,0,0);
                 assertFalse(rm.canIAfford(costBuyDevelopment, true));
                 break;
             case 2:
-                costBuyDevelopment = resourceArray(2,1,0,0,0,0);
-                assertTrue(rm.canIAfford(costBuyDevelopment, true));
+                costBuyDevelopment = resourceArray(5,3,0,0,0,1);
+                assertFalse(rm.canIAfford(costBuyDevelopment, true));
                 break;
             case 3:
                 costBuyDevelopment = new ArrayList<>();
