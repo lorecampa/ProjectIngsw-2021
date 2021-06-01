1ca6996c43a0fed4ec58c88968cb4edfa62c76c7
Matteo <matteo.cordioli@mail.polimi.it>
2021-06-01 17:05:46 +0200

    chore: modify some test

--- src/main/java/it/polimi/ingsw/client/ModelClient.java
+++ src/main/java/it/polimi/ingsw/client/ModelClient.java
@@ -33,8 +33,8 @@ public class ModelClient {
 
     public ModelClient(String username) {
         this.username = username;
-        setUpForDebug();
-        //realSetUp();
+        //setUpForDebug();
+        realSetUp();
     }
 
     private void realSetUp(){
